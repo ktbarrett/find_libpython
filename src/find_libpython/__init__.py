e44c1d3e79290a72c0b9e39aee0a8db2194a73bc
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-02-10 00:11:48 -0600

    Remove old future imports

--- src/find_libpython/__init__.py
+++ src/find_libpython/__init__.py
@@ -25,8 +25,6 @@ Locate libpython associated with this Python executable.
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import print_function, absolute_import
-
 from logging import getLogger
 import ctypes.util
 import functools
